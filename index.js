b0743fb69a3257a5e92acc53bbad9b5de1191bf1
OrNasri <ornasri5@gmail.com>
2023-05-17 10:32:45 +0300

    get

--- index.js
+++ index.js
@@ -25,27 +25,6 @@ async function connectToMongoDb() {
 async function run(){
   var db = await connectToMongoDb();
   var blocks = await getCodeBlocks();
-  // Lobby page route
-  app.get('/', (req, res) => {
-    res.render('lobbyPage', { codeBlocks });
-  });
-  // Code block page route
-  app.get('/codeblock/:title', (req, res) => {
-      const codeBlockTitle = req.params.title;
-      const codeBlock = codeBlocks.find(block => block.title === codeBlockTitle);
-    
-      if (!codeBlock) {
-        return res.status(404).send('Code block not found');
-      }
-    // Check if mentor query parameter is present and set to true
-      const isMentor = req.query.mentor === 'true';
-      res.render('codeblock', { codeBlock, isMentor, codeBlockTitle });
-    });
-
-
-  http.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
 }
 
 run();
@@ -105,26 +84,25 @@ io.on('connection', socket => {
   });
 
 
-// // Lobby page route
-// app.get('/', (req, res) => {
-//   res.render('lobbyPage', { codeBlocks });
-// });
+  // Lobby page route
+  app.get('/', (req, res) => {
+    res.render('lobbyPage', { codeBlocks });
+  });
+  // Code block page route
+  app.get('/codeblock/:title', (req, res) => {
+      const codeBlockTitle = req.params.title;
+      const codeBlock = codeBlocks.find(block => block.title === codeBlockTitle);
+    
+      if (!codeBlock) {
+        return res.status(404).send('Code block not found');
+      }
+    // Check if mentor query parameter is present and set to true
+      const isMentor = req.query.mentor === 'true';
+      res.render('codeblock', { codeBlock, isMentor, codeBlockTitle });
+    });
 
-// // Code block page route
-// app.get('/codeblock/:title', (req, res) => {
-//     const codeBlockTitle = req.params.title;
-//     const codeBlock = codeBlocks.find(block => block.title === codeBlockTitle);
-  
-//     if (!codeBlock) {
-//       return res.status(404).send('Code block not found');
-//     }
-//   // Check if mentor query parameter is present and set to true
-//     const isMentor = req.query.mentor === 'true';
-//     res.render('codeblock', { codeBlock, isMentor, codeBlockTitle });
-//   });
 
-
-// http.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+  http.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
